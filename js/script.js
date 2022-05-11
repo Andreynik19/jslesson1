e4040bdfef699aac6861442c4fd262edd4922058
Andreynik19 <102247475+Andreynik19@users.noreply.github.com>
2022-05-11 15:14:29 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+//1 
+alert('Привет, это работа Гончара Андрея');
+
+// // 2
+// let planetName = 'Земля'
+// console.log(planetName);
+// let myAge = 30
+// console.log(myAge);
+// let BrokenCar = 'Ford';
+// console.log(Ford);
+// let headerColor = 'yellow';
+// console.log(yellow);
+// let howAreYou = confirm('Вы администратор?');
+// console.log(howAreYou);
+
+// // 3
+// let myName = 'Иван';
+// let mySurname = 'Иванович';
+// console.log(`Привет, меня зовут ${myName} ${mySurname}, мне ${myAge} года`);
+
+// // 4
+// let checkingName = prompt('Введите Ваше имя', 'Сhecking');
+// console.log(`Мою работу проверяет ${checkingName}`);
+
+// // 5
+// let str = 'String';
+// console.log(str, typeof(str));
+// let num = 2000;
+// console.log(num, typeof(num));
+// let testSkiped = false;
+// console.log(testSkiped, typeof(testSkiped));
+// let month = undefined;
+// console.log(month, typeof(month));
+// let data = null;
+// console.log(data, typeof(data));
+
+// // 7)
+// let name = 'Ivan';
+// let name = surname;
+// console.log(surname);
